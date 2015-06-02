4d1b5d8cdfa682786aa23429f44e4fea37c1d4f2
DenisLobur <denismachtalles@gmail.com>
2015-06-02 21:11:32 +0300

    added Services implementations

--- assignment3/src/com/example/assignment3/services/WeatherServiceAsync.java
+++ assignment3/src/com/example/assignment3/services/WeatherServiceAsync.java
@@ -2,6 +2,15 @@ package com.example.assignment3.services;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import com.example.assignment3.aidl.WeatherData;
+import com.example.assignment3.aidl.WeatherRequest;
+import com.example.assignment3.aidl.WeatherResults;
+import com.example.assignment3.utils.Utils;
+
+import java.util.List;
 
 /**
  * Created by denis on 6/1/15.
@@ -12,4 +21,27 @@ public class WeatherServiceAsync extends LifecycleLoggingService {
         return new Intent(context,
                 WeatherServiceAsync.class);
     }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mWeatherRequestImpl;
+    }
+
+    private final WeatherRequest.Stub mWeatherRequestImpl = new WeatherRequest.Stub(){
+
+        @Override
+        public void getCurrentWeather(String weather, WeatherResults results) throws RemoteException {
+            final List<WeatherData> weatherResults = Utils.getResults(weather);
+
+            if (weatherResults != null) {
+                Log.d(TAG, ""
+                        + weatherResults.size()
+                        + " results for acronym: "
+                        + weather);
+                results.sendResults(weatherResults);
+            } else {
+                results.sendError("No weather for " + weather + " found");
+            }
+        }
+    };
 }
--- assignment3/src/com/example/assignment3/services/WeatherServiceSync.java
+++ assignment3/src/com/example/assignment3/services/WeatherServiceSync.java
@@ -2,6 +2,15 @@ package com.example.assignment3.services;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import com.example.assignment3.aidl.WeatherCall;
+import com.example.assignment3.aidl.WeatherData;
+import com.example.assignment3.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by denis on 6/1/15.
@@ -12,4 +21,24 @@ public class WeatherServiceSync extends LifecycleLoggingService {
         return new Intent(context,
                 WeatherServiceSync.class);
     }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mWeatherCallImpl;
+    }
+
+    private final WeatherCall.Stub mWeatherCallImpl = new WeatherCall.Stub() {
+
+        @Override
+        public List<WeatherData> getCurrentWeather(String weather) throws RemoteException {
+            final List<WeatherData> weatherResults = Utils.getResults(weather);
+
+            if(weatherResults != null) {
+                Log.d(TAG, "" + weatherResults.size() + " results for weather " + weather);
+                return  weatherResults;
+            } else {
+                return new ArrayList<WeatherData>();
+            }
+        }
+    };
 }
